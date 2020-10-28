9e18ef3cf5d7790d97abec0aedf297ae3d818662
snowthesprite <mpapaya9@gmail.com>
2020-10-28 01:29:50 +0000

    Added the printed assert stuff By the gods I hate that stuff

--- tests/test_euler_estimator.py
+++ tests/test_euler_estimator.py
@@ -4,11 +4,17 @@ from euler_estimator import *
 
 euler = EulerEstimator(derivative = (lambda t: t+1))
 
-assert euler.calc_derivative_at_point((1,4)) == 2
+print('Does the EulerEstimators calc_derivative_at_point correctly calculate the derivative?')
+assert euler.calc_derivative_at_point((1,4)) == 2, 'No, it does not'
+print('Yes, it does!')
+print()
 
+print('Does EulerEstimators step_forward correctly go foward the step size?')
+assert euler.step_forward(point = (1,4), step_size = 0.5) == (1.5, 5), 'No, it does not'
+print('Yes, it does!')
+print()
 
-assert euler.step_forward(point = (1,4), step_size = 0.5) == (1.5, 5)
-
-print(euler.calc_estimated_points(point=(1,4), step_size=0.5, num_steps=4))
-
-assert euler.calc_estimated_points(point=(1,4), step_size=0.5, num_steps=4) == [(1, 4), (1.5, 5), (2, 6.25), (2.5, 7.75), (3, 9.5)]
+print('Does EulerEstimators calc_estimated_points correctly calculate the estimated points?')
+assert euler.calc_estimated_points(point=(1,4), step_size=0.5, num_steps=4) == [(1, 4), (1.5, 5), (2, 6.25), (2.5, 7.75), (3, 9.5)], 'No, it does not'
+print('Yes, it does!')
+print()
