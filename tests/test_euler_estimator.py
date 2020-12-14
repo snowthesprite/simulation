d3a7b34ef015e2aeea20aa7e18e9f4b164ddca9f
snowthesprite <mpapaya9@gmail.com>
2020-12-14 07:46:58 +0000

    Added Asserts Finished!

--- tests/test_euler_estimator.py
+++ tests/test_euler_estimator.py
@@ -34,7 +34,9 @@ euler = EulerEstimator(derivatives = derivatives)
 initial_values = {'A': 0, 'B': 0, 'C': 0}
 initial_point = (0, initial_values)
 
-assert euler.calc_derivative_at_point(initial_point) == {'A': 1, 'B': 0, 'C': 0}
+print('Does everything work?')
+
+assert euler.calc_derivative_at_point(initial_point) == {'A': 1, 'B': 0, 'C': 0}, 'Not calc_derivative_at_point for point_1'
 
 point_2 = euler.step_forward(point = initial_point, step_size = 0.1)
 
@@ -43,11 +45,9 @@ keys = [key for key in rounded]
 for key in keys :
     rounded[key] = round(rounded[key], 1)
 
-assert point_2 == (0.1, {'A': 0.1, 'B': 0, 'C': 0})
+assert point_2 == (0.1, {'A': 0.1, 'B': 0, 'C': 0}), 'point_2 isnt quite right'
 
-print(euler.calc_derivative_at_point(point_2))
-
-assert euler.calc_derivative_at_point(point_2) == {'A': 1.1, 'B': 0.1, 'C': 0}
+assert euler.calc_derivative_at_point(point_2) == {'A': 1.1, 'B': 0.1, 'C': 0}, 'Not calc_derivative_at_point for point_2'
 
 
 point_3 = euler.step_forward(point = point_2, step_size = -0.5)
@@ -57,10 +57,8 @@ keys = [key for key in rounded]
 for key in keys :
     rounded[key] = round(rounded[key], 2)
 
-print(point_3)
-
 assert point_3 == (-0.4, {'A': -0.45, 'B': -0.05, 'C': 0})
-print()
+print(), 'point_3 isnt quite right'
 
 all_points = euler.calc_estimated_points(point=point_3, step_size=2, num_steps=3)
 
@@ -69,4 +67,6 @@ for point in all_points :
     for key in keys :
         rounded[key] = round(rounded[key], 2)
 
-assert all_points == [(-0.4, {'A': -0.45, 'B': -0.05, 'C': 0}),  (1.6, {'A': 0.65, 'B': -1.05, 'C': -0.2}), (3.6, {'A': 3.95, 'B': -1.85, 'C': -4.4}), (5.6, {'A': 13.85, 'B': 2.35, 'C': -11.8})]
+assert all_points == [(-0.4, {'A': -0.45, 'B': -0.05, 'C': 0}),  (1.6, {'A': 0.65, 'B': -1.05, 'C': -0.2}), (3.6, {'A': 3.95, 'B': -1.85, 'C': -4.4}), (5.6, {'A': 13.85, 'B': 2.35, 'C': -11.8})], 'one of the grouped points arent right'
+
+print('Yes it does!')
